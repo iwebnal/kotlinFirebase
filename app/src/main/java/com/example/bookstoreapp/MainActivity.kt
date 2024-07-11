b3e81c134fef2d6cba521a5f4afdb81e75848841
iwebnal <iwebnal@gmail.com>
2024-07-12 00:29:57 +0300

    Get books info

--- app/src/main/java/com/example/bookstoreapp/MainActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/MainActivity.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,6 +42,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val fs = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<Book>())
+    }
+
+    fs.collection("books").get().addOnCompleteListener {
+        task ->
+        if (task.isSuccessful()){
+            list.value = task.result.toObjects(Book::class.java)
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -47,7 +62,19 @@ fun MainScreen() {
                 .fillMaxWidth()
                 .fillMaxHeight(0.8f)
         ) {
-
+            items(list.value) { book ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = book.name, modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth().padding(15.dp)
+                    )
+                }
+            }
         }
         Spacer(modifier = Modifier.height(10.dp))
         Button(
